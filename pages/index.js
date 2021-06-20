00b15368f5e8820e9978f1dacb999bc6466e1412
duddanobre <dudarnobre@gmail.com>
2021-06-20 10:45:33 -0300

    Tabela preenchida com dados do modelo do Ant Design adicionada

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,80 @@
 import React from 'react';
-import { Layout, Menu, Card} from 'antd';
+import { Layout, Menu, Card, Table, Tag, Space} from 'antd';
 
 export default function Home() {
   const { Header, Content } = Layout;
+  const columns = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+      render: text => <a>{text}</a>,
+    },
+    {
+      title: 'Age',
+      dataIndex: 'age',
+      key: 'age',
+    },
+    {
+      title: 'Address',
+      dataIndex: 'address',
+      key: 'address',
+    },
+    {
+      title: 'Tags',
+      key: 'tags',
+      dataIndex: 'tags',
+      render: tags => (
+        <>
+          {tags.map(tag => {
+            let color = tag.length > 5 ? 'geekblue' : 'green';
+            if (tag === 'loser') {
+              color = 'volcano';
+            }
+            return (
+              <Tag color={color} key={tag}>
+                {tag.toUpperCase()}
+              </Tag>
+            );
+          })}
+        </>
+      ),
+    },
+    {
+      title: 'Action',
+      key: 'action',
+      render: (text, record) => (
+        <Space size="middle">
+          <a>Invite {record.name}</a>
+          <a>Delete</a>
+        </Space>
+      ),
+    },
+  ];
   
+  const data = [
+    {
+      key: '1',
+      name: 'John Brown',
+      age: 32,
+      address: 'New York No. 1 Lake Park',
+      tags: ['nice', 'developer'],
+    },
+    {
+      key: '2',
+      name: 'Jim Green',
+      age: 42,
+      address: 'London No. 1 Lake Park',
+      tags: ['loser'],
+    },
+    {
+      key: '3',
+      name: 'Joe Black',
+      age: 32,
+      address: 'Sidney No. 1 Lake Park',
+      tags: ['cool', 'teacher'],
+    },
+  ];
   return (
   <Layout>
     <Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
@@ -18,7 +89,7 @@ export default function Home() {
     <Content style={{ padding: '0 50px', marginTop: 64,  background: '#fff'}}>
       <div style={{ padding: 24, minHeight: 380, background: '#fff', color: '#fff'}}>
       <Card title="Agenda">
-      
+        <Table columns={columns} dataSource={data} />
       </Card>
       </div>
     </Content>
