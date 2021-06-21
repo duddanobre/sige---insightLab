aa2d8c1a02d26720b8d305b2f3c12344ac1731cc
duddanobre <dudarnobre@gmail.com>
2021-06-21 20:57:53 -0300

    Melhora no Header da aplicação, inserção de icone e mudança no tamnho e cor das palavras

--- components/header.js
+++ components/header.js
@@ -1,5 +1,6 @@
 import {Layout,  Menu } from 'antd';
 import { Component } from 'react';
+import {LogoutOutlined} from '@ant-design/icons';
 
 export default class Header extends Component{
     render(){
@@ -8,10 +9,13 @@ export default class Header extends Component{
             <Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
               <div style={{float: 'left', width: '120px', height: '31px', margin: (16, 24, 16, 0), 
                 color: '#fff'}}>
-                  <a href="/"><span>SIGE</span></a> 
+                  <a href="/"><span style={{fontSize: '20px', fontWeight: 'bold', color: '#fff'}}>SIGE</span></a> 
               </div>
               <Menu theme="dark" mode="horizontal" style={{float: 'right'}}>
-                <Menu.Item key="1">Sair</Menu.Item>
+                <Menu.Item key="1" style={{color: '#fff', fontSize: '16px', fontWeight: 400}}>
+                <LogoutOutlined style={{fontSize: '16px', color:'#fff', marginRight: '10px', fontWeight: 400}} />
+                  Sair
+                </Menu.Item>
               </Menu>
               </Header>
         )
