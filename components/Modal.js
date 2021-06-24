67a164a0f8242eb5323b74f44eec618c48d38296
duddanobre <dudarnobre@gmail.com>
2021-06-24 17:50:49 -0300

    Removendo o botao cancel do modal

--- components/Modal.js
+++ components/Modal.js
@@ -6,6 +6,7 @@ export default function CadastrarEvento(props){
   return (
     <div>
       <Modal title="Cadastrar Evento" 
+      cancelButtonProps={{ style: { display: 'none' } }}
       onOk={props.ok}  
       visible={props.visible}
       style={{top: 35}}
