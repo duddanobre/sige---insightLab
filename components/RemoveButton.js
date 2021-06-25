0f5c0754cbffa28c802823b39f3e8199d74ef241
duddanobre <dudarnobre@gmail.com>
2021-06-25 00:09:50 -0300

    Construindo função no react para deletar evento pelo id

--- components/RemoveButton.js
+++ components/RemoveButton.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import {Button} from 'antd';
 import {DeleteOutlined} from '@ant-design/icons'
+import axios from 'axios';
 
 export default function RemoveEvento(){
+    const id = '60d53ebdee0a7a10c8ddeb62';
+
+    async function deleteById(id){
+        try{
+            await axios.delete('/api/', id)
+        }catch (error) {
+         alert('Falha ao deletar, tente novamente');
+        }  
+    }
+    
 
     return(
         <Button size="small" icon={<DeleteOutlined style={{color: '#cc2d37'}} />} 
-            style={{borderColor: '#cc2d37', color: '#cc2d37'}}>
+            style={{borderColor: '#cc2d37', color: '#cc2d37'}} onClick={deleteById(id)}>
         </Button>
     )
 
