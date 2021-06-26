0cd3f749d20cb615b816fcbb88622e698b810390
duddanobre <dudarnobre@gmail.com>
2021-06-26 10:40:47 -0300

    Construção do backend de editar evento

--- pages/api/[id].ts
+++ pages/api/[id].ts
@@ -2,8 +2,8 @@ import { connectToDatabase } from '../../util/mongodb';
 import { NextApiResponse, NextApiRequest } from 'next';
 import mongodb from 'mongodb';
 
-  export default async (req: NextApiRequest, res: NextApiResponse) => {
-    if(req.method === 'DELETE')  {
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+    if(req.method === 'DELETE'){
       const { query: { id } } = req;
       const query = { _id: new mongodb.ObjectID(id) };
 
@@ -11,17 +11,42 @@ import mongodb from 'mongodb';
     
       const collection = db.collection('evento');
 
-    try {
-      await collection.deleteOne(query);
-      
-      return res.status(200).json({
-        success: true,
-        data: { id },
-      });
+      try {
+        await collection.deleteOne(query);
+        
+        return res.status(200).json({
+          success: true,
+          data: { id },
+        });
+      }catch (error) {
+          return res.status(400).json({
+            success: false,
+          });
+        }
+    }  
+
+    if(req.method === 'PUT'){
+      const { _id, nome, horario, local, atividades ,participantes } = req.body;
+
+      const { db } = await connectToDatabase();
+    
+      const collection = db.collection('evento');
+
+      try {
+        await collection.updateOne(
+          { _id: new mongodb.ObjectID(_id) },
+          { $set: { nome, horario, local, atividades ,participantes }});
+
+          return res.status(200).json({
+            success: true,
+            data: { _id, nome, horario, local, atividades ,participantes},
+          });
+
       } catch (error) {
+        console.log(error);
         return res.status(400).json({
           success: false,
         });
       }
-    }   
-  }
+    }
+}
